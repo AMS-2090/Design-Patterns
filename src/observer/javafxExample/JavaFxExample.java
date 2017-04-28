ba9d80819b330ff0cae16db423f397ec59762d27
Arkadiusz <ben4815@gmail.com>
2017-04-28 17:45:11 +0200

    Rework FxExample so that it has a listener class
    ButtonHandler class was added as a listener object in order to
    listen to click events in the buttons (which are subjects).

--- src/observer/javafxExample/JavaFxExample.java
+++ src/observer/javafxExample/JavaFxExample.java
@@ -3,7 +3,7 @@
  * It is set by setOnAction(EventHandler<ActionEvent> event) method in JavaFxExample class.
  * 
  * When the button is fired the handle(ActionEvent event) method
- * from JavaFxExample class is invoked.
+ * from JavaFxExample class is being invoked.
  * 
  * In this way, JavaFxExample class is being notified about changes in buttons.
  */
@@ -13,14 +13,15 @@ package observer.javafxExample;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
-public class JavaFxExample extends Application implements EventHandler<ActionEvent> {
+public class JavaFxExample extends Application {
 	
-	Button button;
+	Button button, button2;
 	
 	public static void main(String[] args) {
 		launch(args);
@@ -31,26 +32,36 @@ public class JavaFxExample extends Application implements EventHandler<ActionEve
 		primaryStage.setTitle("JavaFX Example");
 		
 		button = new Button("Button");
+		button2 = new Button("Button 2");
+		EventHandler<ActionEvent> buttonActionHandler = new ButtonHandler();
 		
 		/*
-		 * 'this' argument means that JavaFxExample class
-		 * will be searched for a handle(ActionEvent event) method
-		 * to provide action handling for button state change. 
+		 * Adding ButtonHandler as the Observer of buttons action 
 		 */
-		button.setOnAction(this);
+		button.setOnAction(buttonActionHandler);
+		button2.setOnAction(buttonActionHandler);
 		
-		StackPane layout = new StackPane();
-		layout.getChildren().add(button);
+		HBox hBox = new HBox(30);
+		hBox.setAlignment(Pos.CENTER);
 		
-		Scene scene = new Scene(layout, 300, 250);
+		hBox.getChildren().add(button);
+		hBox.getChildren().add(button2);
+		
+		Scene scene = new Scene(hBox, 300, 250);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
 
-	@Override
-	public void handle(ActionEvent event) {
-		if (event.getSource() == button) {
-			System.out.println("Hello world!");
+	class ButtonHandler implements EventHandler<ActionEvent> {
+		
+		@Override
+		public void handle(ActionEvent event) {
+			if (event.getSource() == button) {
+				System.out.println("button: Hello world!");
+			}
+			if(event.getSource() == button2) {
+				System.out.println("button 2: Hello world!");
+			}
 		}
 	}
 
