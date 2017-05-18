a5ee60df7e7fdb5ed016d36de292b236913ddfa5
Arkadiusz <ben4815@gmail.com>
2017-05-18 12:39:57 +0200

    Remove unnecessary member of Ingredient class

--- src/decorator/coffee/IngredientDecorator.java
+++ src/decorator/coffee/IngredientDecorator.java
@@ -3,7 +3,6 @@ package decorator.coffee;
 public abstract class IngredientDecorator extends Drink {
 	
 	protected Drink drink;
-	protected String[] description;
 	
 	public abstract String getDescription();
 	
