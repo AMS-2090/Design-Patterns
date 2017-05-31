c793963c9bceac823cc307330e0cde1a41695c40
Arkadiusz <ben4815@gmail.com>
2017-05-31 23:35:45 +0200

    Add RollerBlinds class

---
+++ src/command/homekit/RollerBlinds.java
@@ -0,0 +1,15 @@
+package command.homekit;
+
+public class RollerBlinds {
+
+	public RollerBlinds() {
+	}
+	
+	public void up() {
+		System.out.println("Roller blinds are lifted.");
+	}
+	
+	public void down() {
+		System.out.println("Roller blinds are lowered.");
+	}
+}
