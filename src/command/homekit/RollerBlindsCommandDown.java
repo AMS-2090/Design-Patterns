c603474fa6a1523fb792ed8dab5621a45fc94abc
Arkadiusz <ben4815@gmail.com>
2017-06-01 23:23:00 +0200

    Add RollerBlindsCommand Up/Down classes
    Classes implementing Command interface added for RollerBlinds in order to lift it up and lower it down.

---
+++ src/command/homekit/RollerBlindsCommandDown.java
@@ -0,0 +1,16 @@
+package command.homekit;
+
+public class RollerBlindsCommandDown implements Command {
+
+	private RollerBlinds rollerBlinds;
+
+	public RollerBlindsCommandDown(RollerBlinds rollerBlinds) {
+		this.rollerBlinds = rollerBlinds;
+	}
+	
+	@Override
+	public void execute() {
+		rollerBlinds.down();
+	}
+
+}
---
+++ src/command/homekit/RollerBlindsCommandUp.java
@@ -0,0 +1,16 @@
+package command.homekit;
+
+public class RollerBlindsCommandUp implements Command {
+	
+	private RollerBlinds rollerBlinds;
+
+	public RollerBlindsCommandUp(RollerBlinds rollerBlinds) {
+		this.rollerBlinds = rollerBlinds;
+	}
+	
+	@Override
+	public void execute() {
+		rollerBlinds.up();
+	}
+
+}
