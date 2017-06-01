c95372d375e6b9908d8595ee3e7b9c708422acdc
Arkadiusz <ben4815@gmail.com>
2017-06-01 23:25:32 +0200

    Add simple body for HomeKit class
    HomeKit class got a methods for assigning command and lanuch it.

--- src/command/homekit/HomeKit.java
+++ src/command/homekit/HomeKit.java
@@ -2,4 +2,16 @@ package command.homekit;
 
 public class HomeKit {
 
+	Command slot;
+	
+	public HomeKit() {
+	}
+
+	public void setCommand(Command command) {
+		slot = command;
+	}
+	
+	public void launch() {
+		slot.execute();
+	}
 }
