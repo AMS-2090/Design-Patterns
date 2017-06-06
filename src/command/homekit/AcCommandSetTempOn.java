c7fa78f78ab3ffd4b3d876c5c80588d21e4640e8
Arkadiusz <ben4815@gmail.com>
2017-06-06 14:25:06 +0200

    Add AcCommandSetTempOn class
    Command class to setting temperature of AC.

---
+++ src/command/homekit/AcCommandSetTempOn.java
@@ -0,0 +1,25 @@
+package command.homekit;
+
+public class AcCommandSetTempOn implements Command {
+
+	AirConditioner airConditioner;
+	private int temperature;
+	private int prevTemperature;
+	
+	public AcCommandSetTempOn(AirConditioner airConditioner, int temperature) {
+		this.airConditioner = airConditioner;
+		this.temperature = temperature;
+	}
+	
+	@Override
+	public void execute() {
+		prevTemperature = airConditioner.getTemperature();
+		airConditioner.setTemperature(temperature);
+	}
+
+	@Override
+	public void undo() {
+		airConditioner.setTemperature(prevTemperature);
+	}
+
+}
