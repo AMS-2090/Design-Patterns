0226807e8ff8b9113966c9cd3854c26ec6443662
Arkadiusz <ben4815@gmail.com>
2017-06-16 15:53:08 +0200

    Finish start() method
    - entire starting procedure,
    - camera, remote controller and gps added.

--- src/facade/DroneFacade.java
+++ src/facade/DroneFacade.java
@@ -8,28 +8,50 @@ public class DroneFacade {
 	private CommunicationModule comm;
 	private GPSmodule gps;
 	private VideoCamera camera;
+	private RemoteController rc;
 	
 	public DroneFacade(PowerSupply power,
 					   SensorsModule sensors,
 					   ActuatorsModule actuators,
 					   FlightControllerBoard fcb,
-					   CommunicationModule comm) {
+					   CommunicationModule comm,
+					   VideoCamera camera,
+					   RemoteController rc,
+					   GPSmodule gps) {
 		
 		this.power = power;
 		this.sensors = sensors;
 		this.actuators = actuators;
 		this.fcb = fcb;
 		this.comm = comm;
+		this.camera = camera;
+		this.rc = rc;
+		this.gps = gps;
 	}
 	
 	public void start() {
-		System.out.println("Drone is preparing to start...");
+		System.out.println("\n* Drone is preparing to start...");
 		
-		if(power.getState() == false) {
+		// battery
+		if(!power.getState()) {
 			power.on();
 		}
+		// Flight Controller Board
+		if(fcb.powerOn(power)) {
+			// sensors
+			fcb.setUpSensors(sensors);
+			// actuators
+			fcb.setUpActuators(actuators);
+			// camera
+			fcb.setUpCamera(camera);
+			// communication
+			fcb.setUpCommuniaction(comm, rc);
+			// GPS
+			fcb.setUpGps(gps);
+			
+			System.out.println(">>> Up! <<<");
+		}
 		
-		sensors.setUpSensors();
 	}
 	
 	public void land() {
